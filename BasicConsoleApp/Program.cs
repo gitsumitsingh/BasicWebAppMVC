3bc817d04404377c4d367a2dd24b7c62b2ac6566
SumitSPDC <sumit.singh@powerweave.com>
2024-06-11 15:18:43 +0530

    Adding Abstract

--- BasicConsoleApp/Program.cs
+++ BasicConsoleApp/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Welcome to oops basic concepts");
+/// <summary>
+/// you can write static void Main() line outside the Program class also like below and comment class Program
+/* https://github.com/SonicTheCat/CSharp-OOP-Basics/blob/master/03.Working%20With%20Abstraction%20-%20Lab/03.StudentSystem/StudentSystem.cs
+ * Console.WriteLine("Please Enter type, name, age & grade like below Eg.");
+Console.WriteLine("Create Raj 28 5");
+Console.WriteLine("Show Raj");
+StudentSystem studentSystem = new StudentSystem();
+
+string input;
+while ((input = Console.ReadLine()) != "Exit")
+{
+    studentSystem.ParseCommand(input);
+}
+*/
+/// </summary>
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter type, name, age & grade like below Eg.");
+        Console.WriteLine("Create Raj 28 5");
+        Console.WriteLine("Show Raj");
+        StudentSystem studentSystem = new StudentSystem();
+
+        string input;
+        while ((input = Console.ReadLine()) != "Exit")
+        {
+            studentSystem.ParseCommand(input);
+        }
+    }
+}
+
+
